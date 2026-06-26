15e1493def4a5dc185f5d418fb353e82c6ba0a3d
Shubham Vishwakarma <shubhampc40k@gmail.com>
2026-06-26 21:09:56 +0530

    Add files via upload

---
+++ 2026-06-19_SQL_080.sql
@@ -0,0 +1,12 @@
+﻿-- 100 Days SQL Practice
+--Phase 5 — Advanced SQL (Day 76–100)
+
+--Day 80
+--Use CTE with aggregation:
+--Calculate total Quantity per ProductID from TransactionHistory using CTE.
+WITH ProductQuantity AS(
+SELECT ProductID, SUM(Quantity) AS TotalQuantity
+FROM TransactionHistory
+GROUP BY ProductID
+)
+SELECT * FROM ProductQuantity;
